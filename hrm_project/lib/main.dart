993a4ff315a4f7c0bf93bce7334b5cac8fba2887
chanhooop <cksghwwkd@gmail.com>
2023-10-27 11:03:33 +0900

    add project

---
+++ hrm_project/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hrm_project/views/main_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+          title: 'Flutter Animations Masterclass',
+          theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.light(
+                primary: Colors.blue,
+              )),
+          home: const MainView()),
+    );
+  }
+}
