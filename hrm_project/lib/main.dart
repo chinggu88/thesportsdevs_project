39cf95a668eed8d6327a5542974206360d133469
이강훈 <chinggu88@gmail.com>
2023-10-31 10:45:58 +0900

    log 추가중

--- hrm_project/lib/main.dart
+++ hrm_project/lib/main.dart
@@ -1,33 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hrm_project/constant/logger.dart';
 import 'package:hrm_project/riverpod/view/main_view.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
-}
-
-class MyApp extends ConsumerStatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _MyApp();
-}
-
-class _MyApp extends ConsumerState {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Animations Masterclass',
-        theme: ThemeData(
-            useMaterial3: true,
-            colorScheme: ColorScheme.light(
-              primary: Colors.blue,
-            )),
-        home: const MainView());
-  }
+  // runApp(ProviderScope(observers: [logger()], child: MyApp()));
+  runApp(ProviderScope(
+      observers: [logger()],
+      child: MaterialApp(
+          title: 'Flutter Animations Masterclass',
+          theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.light(
+                primary: Colors.blue,
+              )),
+          home: const MainView())));
 }
